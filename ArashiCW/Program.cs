4f3498adb3e18fcf91f92c9d88ffb544035611f8
Milkey Tan <819774989@qq.com>
2020-03-02 13:00:28 +0800

    sth

---
+++ ArashiCW/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ArashiCW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ws = new ClientWebSocket();
+            ws.ConnectAsync(new Uri("ws://127.0.0.1:3030"), CancellationToken.None);
+            Console.ReadLine();
+        }
+    }
+}
--- ArashiW/Program.cs
+++ ArashiW/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Net.WebSockets;
+using ARSoft.Tools.Net.Dns;
+using Fleck;
 
 namespace ArashiW
 {
@@ -10,7 +10,27 @@ namespace ArashiW
     {
         static void Main(string[] args)
         {
+            var server = new WebSocketServer("ws://0.0.0.0:3030");
+            server.Start(ws =>
+            {
+                ws.OnOpen = () => Console.WriteLine(ws.ConnectionInfo.ClientIpAddress + "Open!");
+                ws.OnClose = () => Console.WriteLine(ws.ConnectionInfo.ClientIpAddress + "Close!");
+                ws.OnBinary = msg =>
+                {
+                    var dnsQMsg = DnsMessage.Parse(msg);
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    dnsQMsg.Questions.ForEach(o => Console.WriteLine("Qu:" + o));
+                    var dnsMsg =
+                        new DnsClient(IPAddress.Parse("8.8.8.8"), 1000).SendMessage(dnsQMsg);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    dnsMsg.AnswerRecords.ForEach(o => Console.WriteLine("An:" + o));
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    dnsMsg.AuthorityRecords.ForEach(o => Console.WriteLine("Au:" + o));
+                    Console.ForegroundColor = ConsoleColor.Green;
+                };
+            });
 
+            Console.ReadLine();
         }
     }
 }
