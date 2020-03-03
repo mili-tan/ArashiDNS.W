ce993e3e502fe369a4f7fa8917ffef52ecc011d8
Milkey Tan <819774989@qq.com>
2020-03-03 20:49:09 +0800

    sth

--- ArashiW/Program.cs
+++ ArashiW/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Net;
 using ARSoft.Tools.Net.Dns;
 using Fleck;
@@ -11,23 +12,32 @@ namespace ArashiW
         {
             DNSEncoder.Init();
             var server = new WebSocketServer("ws://0.0.0.0:3030");
+            server.ListenerSocket.NoDelay = true;
             server.Start(ws =>
             {
                 ws.OnOpen = () => Console.WriteLine(ws.ConnectionInfo.ClientIpAddress + ":Open!");
                 ws.OnClose = () => Console.WriteLine(ws.ConnectionInfo.ClientIpAddress + ":Close!");
                 ws.OnBinary = msg =>
                 {
-                    var dnsQMsg = DnsMessage.Parse(msg);
-                    Console.ForegroundColor = ConsoleColor.Cyan;
-                    dnsQMsg.Questions.ForEach(o => Console.WriteLine("Qu:" + o));
-                    var dnsMsg =
-                        new DnsClient(IPAddress.Parse("8.8.8.8"), 1000).SendMessage(dnsQMsg);
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    dnsMsg.AnswerRecords.ForEach(o => Console.WriteLine("An:" + o));
-                    Console.ForegroundColor = ConsoleColor.DarkGreen;
-                    dnsMsg.AuthorityRecords.ForEach(o => Console.WriteLine("Au:" + o));
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    ws.Send(DNSEncoder.Encode(dnsMsg));
+                    using (var bgWorker = new BackgroundWorker())
+                    {
+                        bgWorker.DoWork += (sender, eventArgs) =>
+                        {
+                            var dnsQMsg = DnsMessage.Parse(msg);
+                            var dnsRMsg =
+                                new DnsClient(IPAddress.Parse("8.8.8.8"), 1000).SendMessage(dnsQMsg);
+                            ws.Send(DNSEncoder.Encode(dnsRMsg));
+
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            dnsQMsg.Questions.ForEach(o => Console.WriteLine("Qu:" + o));
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            dnsRMsg.AnswerRecords.ForEach(o => Console.WriteLine("An:" + o));
+                            Console.ForegroundColor = ConsoleColor.DarkGreen;
+                            dnsRMsg.AuthorityRecords.ForEach(o => Console.WriteLine("Au:" + o));
+                            Console.ForegroundColor = ConsoleColor.Green;
+                        };
+                        bgWorker.RunWorkerAsync();
+                    }
                 };
             });
 
