f3ad242f2fb1939b1c870803f9d45343f2c6348c
Milkey Tan <819774989@qq.com>
2020-03-11 19:15:47 +0800

    sth

--- ArashiCW/Program.cs
+++ ArashiCW/Program.cs
@@ -66,7 +66,7 @@ namespace ArashiCW
                                             dnsRMsg.AuthorityRecords.ForEach(Console.WriteLine);
                                         };
                                         bgw.RunWorkerAsync();
-                                    };
+                                    }
                                 };
                                 item.Key.RunWorkerAsync();
 
