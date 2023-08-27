3a64efdab8833778809aa88a585181df4535223d
Milkey Tan <819774989@qq.com>
2023-08-27 20:08:50 +0800

    sth

--- ArashiDNS.CW/Program.cs
+++ ArashiDNS.CW/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Net;
 using System.Net.WebSockets;
 using System.Threading;
@@ -11,65 +12,91 @@ namespace ArashiDNS
     {
         static void Main(string[] args)
         {
-            using (DnsServer dnsServer = new DnsServer(IPAddress.Any, 10, 10))
+            using var dnsServer = new DnsServer(IPAddress.Any, 10, 10);
+            DNSEncoder.Init();
+
+            var wsPool = new ObjectPool<ClientWebSocket>(() =>
             {
-                DNSEncoder.Init();
-
                 var ws = new ClientWebSocket();
-                ws.ConnectAsync(new Uri("ws://127.0.0.1:3030"), CancellationToken.None).Wait();
+                ws.ConnectAsync(new Uri("ws://127.0.0.1:22332"), CancellationToken.None).Wait();
+                return ws;
+            });
 
-                async Task OnDnsServerOnQueryReceived(object sender, QueryReceivedEventArgs e)
-                {
-                    await Task.Run(() => {
-                        try
-                        {
-                            if (!(e.Query is DnsMessage query)) return;
-                            if (ws.State == WebSocketState.Aborted)
-                            {
-                                ws.Dispose();
-                                ws = new ClientWebSocket();
-                                ws.ConnectAsync(new Uri("ws://127.0.0.1:3030"), CancellationToken.None)
-                                    .Wait();
-                            }
-                            else if (ws.State == WebSocketState.Closed)
-                                ws.ConnectAsync(new Uri("ws://127.0.0.1:3030"), CancellationToken.None)
-                                    .Wait();
-
-                            var dnsBytes = new byte[512];
-                            lock (ws)
-                            {
-                                ws.SendAsync(new ArraySegment<byte>(DNSEncoder.Encode(query)),
-                                    WebSocketMessageType.Binary, true, CancellationToken.None).Wait();
-                                ws.ReceiveAsync(new ArraySegment<byte>(dnsBytes), CancellationToken.None).Wait();
-                            }
-                            var dnsRMsg = DnsMessage.Parse(dnsBytes);
-                            e.Response = dnsRMsg;
-                            Task.Run(() =>
-                            {
-                                dnsRMsg.AnswerRecords.ForEach(Console.WriteLine);
-                                dnsRMsg.AuthorityRecords.ForEach(Console.WriteLine);
-                            });
-                        }
-                        catch (Exception exception)
-                        {
-                            Console.WriteLine(exception);
-                        }
-                    });
-                }
-
-                dnsServer.QueryReceived += OnDnsServerOnQueryReceived;
-                dnsServer.Start();
-                Console.WriteLine(@"-------ARASHI   DNS------");
-                Console.WriteLine(@"-------DOTNET ALPHA------");
-
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine(@"ArashiDNS Server Running");
-
-                Console.WriteLine(@"Press any key to stop dns server");
-                Console.WriteLine("------------------------");
-                Console.ReadLine();
-                Console.WriteLine("------------------------");
+            for (var i = 0; i < 3; i++)
+            {
+                wsPool.Return(wsPool.Get());
             }
+
+            async Task OnDnsServerOnQueryReceived(object sender, QueryReceivedEventArgs e)
+            {
+                try
+                {
+                    if (e.Query is not DnsMessage query) return;
+                    var ws = wsPool.Get();
+                    switch (ws.State)
+                    {
+                        case WebSocketState.Aborted:
+                            ws.Dispose();
+                            ws = new ClientWebSocket();
+                            ws.ConnectAsync(new Uri("ws://127.0.0.1:3030"), CancellationToken.None)
+                                .Wait();
+                            break;
+                        case WebSocketState.None:
+                        case WebSocketState.Closed:
+                            ws.ConnectAsync(new Uri("ws://127.0.0.1:3030"), CancellationToken.None)
+                                .Wait();
+                            break;
+                    }
+
+                    var dnsBytes = new byte[512];
+
+                    await ws.SendAsync(new ArraySegment<byte>(DNSEncoder.Encode(query)),
+                        WebSocketMessageType.Binary, true, CancellationToken.None);
+                    await ws.ReceiveAsync(new ArraySegment<byte>(dnsBytes), CancellationToken.None);
+                    wsPool.Return(ws);
+
+                    var dnsRMsg = DnsMessage.Parse(dnsBytes);
+                    Task.Run(() =>
+                    {
+                        dnsRMsg.AnswerRecords.ForEach(Console.WriteLine);
+                        dnsRMsg.AuthorityRecords.ForEach(Console.WriteLine);
+                    });
+                    e.Response = dnsRMsg;
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception);
+                }
+            }
+
+            dnsServer.QueryReceived += OnDnsServerOnQueryReceived;
+            dnsServer.Start();
+            Console.WriteLine(@"-------ARASHI   DNS------");
+            Console.WriteLine(@"-------DOTNET ALPHA------");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(@"ArashiDNS Server Running");
+
+            Console.WriteLine(@"Press any key to stop dns server");
+            Console.WriteLine("------------------------");
+            Console.ReadLine();
+            Console.WriteLine("------------------------");
+        }
+
+        public class ObjectPool<T>
+        {
+            private readonly ConcurrentBag<T> _objects;
+            private readonly Func<T> _objectGenerator;
+
+            public ObjectPool(Func<T> objectGenerator)
+            {
+                _objectGenerator = objectGenerator ?? throw new ArgumentNullException(nameof(objectGenerator));
+                _objects = new ConcurrentBag<T>();
+            }
+
+            public T Get() => _objects.TryTake(out T item) ? item : _objectGenerator();
+
+            public void Return(T item) => _objects.Add(item);
         }
     }
 }
--- ArashiDNS.W/Program.cs
+++ ArashiDNS.W/Program.cs
@@ -11,7 +11,7 @@ namespace ArashiDNS.W
         static void Main(string[] args)
         {
             DNSEncoder.Init();
-            var server = new WebSocketServer("ws://0.0.0.0:3030");
+            var server = new WebSocketServer("ws://0.0.0.0:22332");
             server.ListenerSocket.NoDelay = true;
             server.Start(ws =>
             {
@@ -19,25 +19,19 @@ namespace ArashiDNS.W
                 ws.OnClose = () => Console.WriteLine(ws.ConnectionInfo.ClientIpAddress + ":Close!");
                 ws.OnBinary = msg =>
                 {
-                    using (var bgWorker = new BackgroundWorker())
-                    {
-                        bgWorker.DoWork += (sender, eventArgs) =>
-                        {
-                            var dnsQMsg = DnsMessage.Parse(msg);
-                            var dnsRMsg =
-                                new DnsClient(IPAddress.Parse("8.8.8.8"), 1000).SendMessage(dnsQMsg);
-                            ws.Send(DNSEncoder.Encode(dnsRMsg));
+                    var dnsQMsg = DnsMessage.Parse(msg);
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    dnsQMsg.Questions.ForEach(o => Console.WriteLine("Qu:" + o));
 
-                            Console.ForegroundColor = ConsoleColor.Cyan;
-                            dnsQMsg.Questions.ForEach(o => Console.WriteLine("Qu:" + o));
-                            Console.ForegroundColor = ConsoleColor.Green;
-                            dnsRMsg.AnswerRecords.ForEach(o => Console.WriteLine("An:" + o));
-                            Console.ForegroundColor = ConsoleColor.DarkGreen;
-                            dnsRMsg.AuthorityRecords.ForEach(o => Console.WriteLine("Au:" + o));
-                            Console.ForegroundColor = ConsoleColor.Green;
-                        };
-                        bgWorker.RunWorkerAsync();
-                    }
+                    var dnsRMsg =
+                        new DnsClient(IPAddress.Parse("119.29.29.29"), 100).SendMessage(dnsQMsg);
+                    ws.Send(DNSEncoder.Encode(dnsRMsg));
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    dnsRMsg.AnswerRecords.ForEach(o => Console.WriteLine("An:" + o));
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    dnsRMsg.AuthorityRecords.ForEach(o => Console.WriteLine("Au:" + o));
+                    Console.ForegroundColor = ConsoleColor.Green;
                 };
             });
 
